e5f2c697be189a34d92f1f1b4d6b3482526846aa
Furkan Söylemez <furkansoylemez@outlook.com>
2023-04-08 02:30:17 +0300

    Update login_page.dart

--- lib/feature/login/view/login_page.dart
+++ lib/feature/login/view/login_page.dart
@@ -1,8 +1,9 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:birsu/core/app_router/app_router.dart';
 import 'package:birsu/core/extension/context_extensions.dart';
 import 'package:birsu/core/extension/widget_extensions.dart';
-import 'package:birsu/core/helper/form_validate_helper.dart';
-import 'package:birsu/core/helper/toast_helper.dart';
+import 'package:birsu/core/helper/dialog_helper.dart';
+import 'package:birsu/core/helper/other_helpers.dart';
 import 'package:birsu/feature/login/logic/login_notifier.dart';
 import 'package:birsu/widgets/custom_spacer.dart';
 import 'package:birsu/widgets/form_fields/email_form_field.dart';
@@ -17,20 +18,22 @@ class LoginPage extends ConsumerWidget {
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final dialogHelper = ref.watch(dialogHelperProvider);
+
     final loginStatus = ref.watch(
       loginNotifierProvider.select((state) => state.loginStatus),
     );
 
-    final toastHelper = ref.watch(toastHelperProvider);
-
-    ref.listen(
-      loginNotifierProvider,
-      (_, state) {
-        if (state.loginStatus is AsyncError && state.loginStatus.hasError) {
-          toastHelper.showSnackBar(context, state.loginStatus.error.toString());
-        }
-      },
-    );
+    ref.listen(loginNotifierProvider, (prev, next) {
+      dialogHelper.onAsyncErrorShowDialog(
+        context,
+        prev?.loginStatus,
+        next.loginStatus,
+      );
+      onAsyncSuccess(next.loginStatus, () {
+        context.router.replace(const HomeRoute());
+      });
+    });
     return Scaffold(
       body: Form(
         autovalidateMode: AutovalidateMode.onUserInteraction,
@@ -70,6 +73,12 @@ class LoginPage extends ConsumerWidget {
                   );
                 }
               },
+            ),
+            TextButton(
+              onPressed: () {
+                context.router.push(RegisterRoute());
+              },
+              child: Text(context.loc.register),
             ),
           ],
         ).paddingSymmetric(horizontal: 20.w),
