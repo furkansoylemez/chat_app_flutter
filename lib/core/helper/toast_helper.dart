9be1fca89858aa14ec191ecc01e44473153061ae
Furkan Söylemez <furkansoylemez@outlook.com>
2023-04-08 02:30:27 +0300

    Update toast_helper.dart

--- lib/core/helper/toast_helper.dart
+++ lib/core/helper/toast_helper.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'toast_helper.g.dart';
@@ -20,22 +19,5 @@ class ToastHelper {
         duration: Duration(milliseconds: message.length * 100),
       ),
     );
-  }
-
-  void onProviderErrorShowSnackBar<T>(
-    WidgetRef ref,
-    BuildContext context, {
-    required List<ProviderListenable<AsyncValue<T>>> providers,
-  }) {
-    for (final provider in providers) {
-      ref.listen(
-        provider,
-        (_, state) {
-          if (state is AsyncError && state.hasError) {
-            showSnackBar(context, state.error.toString());
-          }
-        },
-      );
-    }
   }
 }
