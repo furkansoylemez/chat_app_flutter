650511f2dc3d3c8d1d1cce50635785476a652c75
Furkan Söylemez <furkansoylemez@outlook.com>
2023-04-07 20:20:34 +0300

    Update app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTheme {
   static ThemeData get light {
     return ThemeData.light(
       useMaterial3: true,
-    ).copyWith();
+    ).copyWith(
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.r),
+        ),
+      ),
+    );
   }
 
   static ThemeData get dark {
