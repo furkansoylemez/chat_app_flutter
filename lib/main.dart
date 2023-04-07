6ae8f94b34ff0697201a6f6a14fb86a6080aa204
Furkan Söylemez <furkansoylemez@outlook.com>
2023-04-07 03:36:44 +0300

    Add firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:birsu/app/app.dart';
+import 'package:birsu/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const ProviderScope(child: App()));
 }
