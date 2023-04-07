1943f78bba11ee230b658a4948e6436bdc6b3570
Furkan Söylemez <furkansoylemez@outlook.com>
2023-04-07 20:19:23 +0300

    Update app_user provider

--- lib/provider/app_user.dart
+++ lib/provider/app_user.dart
@@ -1,8 +1,8 @@
-import 'package:birsu/provider/firebase_auth_provider.dart';
+import 'package:birsu/provider/firebase_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'app_user_provider.g.dart';
+part 'app_user.g.dart';
 
 @riverpod
 class AppUser extends _$AppUser {
