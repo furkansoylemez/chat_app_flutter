f2fd80243d24b459cfa723b492a1fe8c965fe2e7
Furkan Söylemez <furkansoylemez@outlook.com>
2023-04-07 20:19:37 +0300

    Update app_router_provider

--- lib/provider/app_router_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:birsu/core/app_router/app_router.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'app_router_provider.g.dart';
-
-@riverpod
-AppRouter appRouter(AppRouterRef ref) {
-  return AppRouter();
-}
