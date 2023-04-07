fe495e9f95352e9e3177b364765975ab569c6de2
Furkan Söylemez <furkansoylemez@outlook.com>
2023-04-08 02:29:29 +0300

    Update password_form_field.dart

--- lib/widgets/form_fields/password_form_field.dart
+++ lib/widgets/form_fields/password_form_field.dart
@@ -1,3 +1,4 @@
+import 'package:birsu/core/app_constants.dart';
 import 'package:birsu/core/extension/context_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
@@ -6,21 +7,26 @@ class PasswordFormField extends StatelessWidget {
   const PasswordFormField({
     super.key,
     this.onChanged,
+    this.validators,
   });
   final void Function(String)? onChanged;
+  final List<FieldValidator<dynamic>>? validators;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       onChanged: onChanged,
+      obscureText: true,
       decoration: InputDecoration(
         labelText: context.loc.password,
       ),
       validator: MultiValidator([
         RequiredValidator(errorText: context.loc.requiredError),
         MinLengthValidator(
-          8,
-          errorText: context.loc.passwordMinLengthError,
+          AppConstants.passwordMinLength,
+          errorText: context.loc
+              .passwordMinLengthError(AppConstants.passwordMinLength),
         ),
+        if (validators != null) ...validators!,
       ]),
     );
   }
