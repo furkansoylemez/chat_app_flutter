f3891221011f83f149905a2ddc4115ae1d7d17f1
Furkan Söylemez <furkansoylemez@outlook.com>
2023-04-07 20:18:12 +0300

    Create dismissible_body.dart

---
+++ lib/widgets/dismissible_body.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DismissibleBody extends StatelessWidget {
+  const DismissibleBody({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        final currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.focusedChild?.unfocus();
+        }
+      },
+      child: child,
+    );
+  }
+}
