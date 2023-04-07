0c12407d171d609883ecc60b276ac910f08a5741
Furkan Söylemez <furkansoylemez@outlook.com>
2023-04-08 02:31:25 +0300

    Update dialog_helper.dart

--- lib/core/helper/dialog_helper.dart
+++ lib/core/helper/dialog_helper.dart
@@ -1,3 +1,4 @@
+import 'package:birsu/core/extension/context_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -11,8 +12,8 @@ DialogHelper dialogHelper(DialogHelperRef ref) {
 class DialogHelper {
   Future<void> showDialogBox(
     BuildContext context, {
-    required Widget title,
-    required Widget content,
+    required String title,
+    required String content,
     List<Widget>? actions,
     bool barrierDismissible = true,
   }) {
@@ -20,12 +21,35 @@ class DialogHelper {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: title,
-          content: content,
+          title: Text(title),
+          content: Text(content),
           actions: actions,
         );
       },
       barrierDismissible: barrierDismissible,
     );
+  }
+
+  void showErrorDialog(
+    BuildContext context,
+    Object? error,
+  ) {
+    showDialogBox(
+      context,
+      title: context.loc.errorTitle,
+      content: error.toString(),
+    );
+  }
+
+  void onAsyncErrorShowDialog(
+    BuildContext context,
+    AsyncValue<dynamic>? prevAsync,
+    AsyncValue<dynamic> nextAsync,
+  ) {
+    if (prevAsync != nextAsync &&
+        nextAsync is AsyncError &&
+        nextAsync.hasError) {
+      showErrorDialog(context, nextAsync.error);
+    }
   }
 }
