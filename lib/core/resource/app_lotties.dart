341d4cc93a54fa6a156c942c55dee917a2d8932f
Furkan Söylemez <furkansoylemez@outlook.com>
2023-04-08 16:54:25 +0300

    Update lotties

--- lib/core/resource/app_lotties.dart
+++ lib/core/resource/app_lotties.dart
@@ -4,4 +4,9 @@ class AppLotties {
   AppLotties._();
 
   static const String ltCouple = 'assets/lotties/couple.json';
+  static const String ltDarkBackground = 'assets/lotties/dark_background.json';
+  static const String ltLightBackground =
+      'assets/lotties/light_background.json';
+  static const String ltMediatingRabbit =
+      'assets/lotties/mediating_rabbit.json';
 }
